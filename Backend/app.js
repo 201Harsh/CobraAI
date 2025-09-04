287b3467d1fe77a50ef7d68f0bfb4734d64751a3
Harsh <gamerpandeyharsh@gmail.com>
2025-09-04 10:08:10 +0530

    Axios and Cors Setup both Client and Server Side

--- Backend/app.js
+++ Backend/app.js
@@ -5,11 +5,18 @@ ConnectTODB();
 
 const router = require("./routes/user.route");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/users", router);
 
