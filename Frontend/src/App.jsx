eb4e12be07e894bc7c60d5cb3bb930192c75d955
Harsh <gamerpandeyharsh@gmail.com>
2025-09-04 10:12:04 +0530

    React Tostify Setup

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import Router from "./Routing/Router";
+import { ToastContainer , Bounce } from "react-toastify";
 
 const App = () => {
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+        transition={Bounce}
+      />
       <Router />
     </>
   );
