87c75eae0f00a1fdafe0506d8ca4244a0e9e7307
Harsh <gamerpandeyharsh@gmail.com>
2025-09-13 16:34:34 +0530

    ready for vercel deploy

--- Backend/server.js
+++ Backend/server.js
@@ -6,5 +6,6 @@ const http = require("http");
 const server = http.createServer(app);
 
 const port = process.env.PORT || 3000;
-server.listen(port, () => {
-});
+server.listen(port, () => {});
+
+module.exports = app;
