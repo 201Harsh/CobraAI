b45d305145f7972a5cac138796aff1ed1b14f0e8
Harsh <gamerpandeyharsh@gmail.com>
2025-09-13 21:12:07 +0530

    Fixing a Bug

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const ConnectTODB = async () => {
   await mongoose
     .connect(process.env.MONGODB_URL)
-    .then(() => {})
+    .then(() => {
+      console.log("Database connected");
+    })
     .catch((err) => {});
 };
 
