f5f1a0882f4dd9f36a9353217238ed99e36a0475
Harsh <gamerpandeyharsh@gmail.com>
2025-09-13 13:25:28 +0530

    Added AI rate Liimit

--- Backend/routes/ai.route.js
+++ Backend/routes/ai.route.js
@@ -1,9 +1,20 @@
 const router = require("express").Router();
 const UserMiddleware = require("../middlewares/user.middleware");
 const AIController = require("../controllers/ai.controller");
+const RateLimitERMiddleware = require("../middlewares/RateLimter.middleware");
 
-router.post("/chat", UserMiddleware.authUser, AIController.chatWithAI);
+router.post(
+  "/chat",
+  RateLimitERMiddleware.aiLimit,
+  UserMiddleware.authUser,
+  AIController.chatWithAI
+);
 
-router.post("/reviewCode", UserMiddleware.authUser, AIController.reviewCode);
+router.post(
+  "/reviewCode",
+  RateLimitERMiddleware.aiLimit,
+  UserMiddleware.authUser,
+  AIController.reviewCode
+);
 
 module.exports = router;
